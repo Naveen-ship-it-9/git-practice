40c01efc69bbdfe6719f4d926b889b767af5f903
Naveen <naveenkanaparthi09@gmail.com>
2026-03-24 17:09:32 +0530

    adding conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Naveen:: I Venu how are you"
+echo "Venu:: I Naveen I am Fine How are you"
+echo "Naveen:: I am fine venu what are you doing"
+echo "Venu:: I am learning devops"
+

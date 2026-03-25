a10734238130476e5d1efb018bacfcfb93eb627d
Naveen <naveenkanaparthi09@gmail.com>
2026-03-25 13:17:24 +0530

    array

--- 07-array.sh
+++ 07-array.sh
@@ -3,8 +3,8 @@
 FRUITS=("APPLE" "ORANGE" "KIWI")
 
 echo "first fruit is: ${FRUIT[0]}
-echo "2nd fruit is: ${FRUIT[1]}
-echo "3rd fruit is: ${FRUIT[2]}
+echo "second fruit is: ${FRUIT[1]}
+echo "thrid fruit is: ${FRUIT[2]}
 
 
 echo "all fruits: $FRUIT[@]

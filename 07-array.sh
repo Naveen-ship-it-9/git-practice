77342e30216ff70ed975d82c2c93c1eb7a38bf58
Naveen <naveenkanaparthi09@gmail.com>
2026-03-25 12:33:41 +0530

    array

---
+++ 07-array.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+FRUITS= ("apple" "orange" "bananna" "kiwi") #arry
+
+echo "first fruit is:: ${FRUIT[0]}
+
+echo "2nd fruit is:: ${FRUIT[1]}
+
+echo "3rd fruit is:: ${FRUIT[2]}
+
+echo "all fruits:: $FRUIT[@]

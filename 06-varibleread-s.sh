a9f245c6c7513109ad3d46caa7f9a3f2e7e53a80
Naveen <naveenkanaparthi09@gmail.com>
2026-03-25 08:37:42 +0530

    varible read command without visible

---
+++ 06-varibleread-s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "please ask user name::"
+read -s USERNAME
+echo "user entered is::$USERNAME"
+echo "please ask password::"
+read -s password
+echo "user entered password::$PASSWORD"

ff69094fc9a589b673c6dce8d2a2ff1f5eca97e4
Naveen <naveenkanaparthi09@gmail.com>
2026-03-25 07:54:14 +0530

    updated varible

--- 04-variblesoutsideips.sh
+++ 04-variblesoutsideips.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERSON1=$1
 PERSON2=$2
-echo "PERSON1:: I $PERSON2 how are you"
+echo "$ERSON1:: I $PERSON2 how are you"
 echo "$PERSON2:: I $PERSON1 I am Fine How are you"
 echo "$PERSON1:: I am fine $PERSON2 what are you doing"
 echo "$PERSON2:: I am learning devops"

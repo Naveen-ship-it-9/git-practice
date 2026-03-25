812c9c09b0d7cc2a933dd018fb000a7debcfe53d
Naveen <naveenkanaparthi09@gmail.com>
2026-03-25 13:53:46 +0530

    array

--- 07-Arrya.sh
+++ 07-Arrya.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-FRUITS=("APPLE" "KIWI" "ORANGE")
+#FRUITS=("APPLE" "KIWI" "ORANGE")
 
 #echo "first fruit is: ${FRUITS[0]}"
 #echo "second fruit is: ${FRUITS[1]}"
 #echo "Third fruit is: ${FRUITS[2]}"
 
 #echo "all fruits is: ${FRUITS[@]}"
+
+FRUITS1=APPLE
+FRUITS2=KIWI
+FRUITS3=ORANGE
 
 echo "first fruit is: $FRUITS0"
 echo "second fruit is: $FRUITS1"

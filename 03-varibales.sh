9ec8de9155797b3e91aa9190e6fd3dba90526feb
Naveen <naveenkanaparthi09@gmail.com>
2026-03-25 07:07:47 +0530

    varibles

---
+++ 03-varibales.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Joji
+PERSON2=matti
+echo "$PERSON1:: I $PERSON2 how are you"
+echo "$PERSON2:: I $PERSON1 I am Fine How are you"
+echo "$PERSON1:: I am fine $PERSON2 what are you doing"
+echo "$PERSON2:: I am learning devops"

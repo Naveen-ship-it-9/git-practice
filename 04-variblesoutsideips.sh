695cadbc30aa551d5aedc0067104b60e91d6bb9c
Naveen <naveenkanaparthi09@gmail.com>
2026-03-27 19:36:50 +0530

    pratice purpose again write a code

--- 04-variblesoutsideips.sh
+++ 04-variblesoutsideips.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# PERSON1=$1
-# PERSON2=$2
-# echo "$PERSON1:: I $PERSON2 how are you"
-# echo "$PERSON2:: I $PERSON1 I am Fine How are you"
-# echo "$PERSON1:: I am fine $PERSON2 what are you doing"
-# echo "$PERSON2:: I am learning devops"
-
-
-
 PERSON1=$1
 PERSON2=$2
+echo "$PERSON1:: I $PERSON2 how are you"
+echo "$PERSON2:: I $PERSON1 I am Fine How are you"
+echo "$PERSON1:: I am fine $PERSON2 what are you doing"
+echo "$PERSON2:: I am learning devops"
+
+
+
+# PERSON1=$1
+# PERSON2=$2
 
 
 
 
-echo "fruits name:$PERSON1"
-echo "fruit name:$PERSON2"
+# echo "fruits name:$PERSON1"
+# echo "fruit name:$PERSON2"

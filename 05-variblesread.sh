23e8a1c8a5506546369f260211286706659b380f
Naveen <naveenkanaparthi09@gmail.com>
2026-03-27 19:20:47 +0530

    pratice purpose again write a code

--- 05-variblesread.sh
+++ 05-variblesread.sh
@@ -8,9 +8,9 @@
 #echo "user entered is::$USERNAME"
 
 
-FRUITS=("APPLE" "BANNAN" "MANGO" )
+FRUITS=("$1" "$2" "$3" )
 
 
-echo "1st fruit name:${FRUITS[0]}"
-echo "2nd fruit name:${FRUITS[1]}"
-echo "3rd fruit name:${FRUITS[2]}"
+echo "1st fruit name:${FRUITS[$1]}"
+echo "2nd fruit name:${FRUITS[$2]}"
+echo "3rd fruit name:${FRUITS[$3]}"

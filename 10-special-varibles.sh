44d557ba3710f4dbdc44e1a6c126c753fe74babb
Naveen <naveenkanaparthi09@gmail.com>
2026-03-28 09:07:11 +0530

    special varibles

--- 10-special-varibles.sh
+++ 10-special-varibles.sh
@@ -4,3 +4,6 @@ echo "All varibles passed to the script:$@"
 echo "number of varibles passed to:$#"
 echo "script name:$0"
 echo "current working dir:$PWD"
+echo "home dir of user:$HOME"
+echo "PID of the script excuting now:$$"
+

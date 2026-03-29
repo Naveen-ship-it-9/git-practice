1eb215b8c41faa0b97a1237b8104c7000e422444
Naveen <naveenkanaparthi09@gmail.com>
2026-03-29 23:53:55 +0530

    oddnumber

---
+++ 14-oddnumber.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# echo "given number is:"
+# read a
+
+# if (( a % 2 != 0 ))
+# then
+#     echo "given number is odd"
+# else
+#     echo "given number is not odd"
+# fi
+
+#using another style
+
+read y
+
+if ( $y -nq 0 )
+then
+    echo "given number is odd"
+else
+    echo "given number is not odd"
+fi
+
+

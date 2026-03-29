964ff3580b1cb896db4e33c72280c9ed68fd3162
Naveen <naveenkanaparthi09@gmail.com>
2026-03-30 00:06:16 +0530

    pratice

--- 14-oddnumber.sh
+++ 14-oddnumber.sh
@@ -14,7 +14,8 @@
 
 read y
 
-if [ $((y %2)) -ne 0 ]
+if [ $((y %2)) -ne 0 ] 
+#if [(( $y %2 )) -ne 0] this not not wok syntax not correct always right like this if [ $((y %2)) -ne 0 ]
 then
     echo "given number is odd"
 else

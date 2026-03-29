abd44efc243661be44d1cc41b7432e5930fd2842
Naveen <naveenkanaparthi09@gmail.com>
2026-03-29 13:30:51 +0530

    in conditions one number is inside one number is out side

--- 11-condations.sh
+++ 11-condations.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-INT=20
+#takeing the number in side script
+# INT=20
 
-if [ $INT -gt 30 ]
+# if [ $INT -gt 30 ]
+
+# then
+#     echo "given number is greaterthan 30"
+# else
+#     echo "given number is leass than 30"
+
+# fi
+
+NUMBER=$1
+
+if [ $NUMBER -gt 50 ]
 
 then
-    echo "given number is greaterthan 30"
+    echo "given  number is greater then 50"
 else
-    echo "given number is leass than 30"
-
+    echo "given number is less than 50"
 fi
-    
 

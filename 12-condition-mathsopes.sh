776754dca7728836448ae69b69c1b1f50e4d8ca5
Naveen <naveenkanaparthi09@gmail.com>
2026-03-30 08:43:44 +0530

    checking with diff style

--- 12-condition-mathsopes.sh
+++ 12-condition-mathsopes.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# echo "given number is:"
+# read x
+# read y
+
+# if (( x > y))
+# then
+#     echo "given number is greater than"
+# else
+#     echo "given number is leass then"
+# fi
+
 echo "given number is:"
 read x
 read y
 
-if (( x > y))
+if [ x > y ]
 then
     echo "given number is greater than"
 else

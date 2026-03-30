c45142f45abd255dede655c8419906be0525a170
Naveen <naveenkanaparthi09@gmail.com>
2026-03-30 12:29:33 +0530

    printing user id with diff ways

--- 16-printinguserID-Installation.sh
+++ 16-printinguserID-Installation.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-USER=$(id -u)
+#printing user Id
 
+# USER=$(id -u)
+
+# echo "user id is:$USER"
+
+# #printing user id root or not means 0 means root othere than 0 not root
+
+USER=$(id -u)
 echo "user id is:$USER"
+
+if [ $USER -ne 0 ]
+then
+    echo "print user have no root access"
+else
+    echo "print user have root access"
+fi

becd0bbb81e4c4fc126e5af8711dd84a5b42e8c2
Naveen <naveenkanaparthi09@gmail.com>
2026-03-31 08:16:05 +0530

    git&sql install

--- 16-printinguserID-Installation.sh
+++ 16-printinguserID-Installation.sh
@@ -8,8 +8,19 @@
 
 # #printing user id root or not means 0 means root othere than 0 not root
 
-USER=$1
-read USER
+# USER=$1
+# read USER
+# echo "user id is:$USER"
+
+# if [ $USER -ne 0 ]
+# then
+#     echo "print user have no root access"
+# else
+#     echo "print user have root access"
+# fi
+
+USER=$(id -u)
+
 echo "user id is:$USER"
 
 if [ $USER -ne 0 ]
---
+++ 17-Installation-git&mysql.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER=$(id -u)
+read USER
+if =[ $USER -ne 0 ]
+then
+    "user don't have root access"
+else
+    "user have root access"
+

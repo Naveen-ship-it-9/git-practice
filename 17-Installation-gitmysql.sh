6784d85359745da17213817d817a13e2cc015422
Naveen <naveenkanaparthi09@gmail.com>
2026-03-31 08:48:32 +0530

    git and sql install

--- 17-Installation-gitmysql.sh
+++ 17-Installation-gitmysql.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-USERID=$(id -u)
-read USERID
-if [ $USERID -ne 0 ]
+USERID=$(id -u) #this command check user id
+read USERID 
+if [ $USERID -ne 0 ] #if check user have root access or not i.e 0 for root access 1 for not root
 then
     echo "user don't have root access"
     exit 1
 else
     echo "user have root access"
 fi
-#if [ ]
+dnf list installed git #list all packages and check git is there or not 
+if [ $? -ne 0 ] #echo $? it tell us previes command true or not if $?=0 then it install git 
+then
+    echo "install git,if it installed say git already installed"
+    dnf install git -y
+    
+else
+    echo "don't install git"
+fi
 

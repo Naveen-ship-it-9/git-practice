290bf6e7c9f3dd810e1fc3f021551f44101df33e
Naveen <naveenkanaparthi09@gmail.com>
2026-03-31 08:25:23 +0530

    git and sql install

--- 17-Installation-gitmysql.sh
+++ 17-Installation-gitmysql.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 read USERID
 if [ $USERID -ne 0 ]
 then
-    "user don't have root access"
+    echo "user don't have root access"
     exit 1
 else
-    "user have root access"
+    echo "user have root access"
 fi
 

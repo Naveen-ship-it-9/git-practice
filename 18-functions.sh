358d5ba2123d2ed111da048ccdea88a05f9e889d
Naveen <naveenkanaparthi09@gmail.com>
2026-04-01 07:35:44 +0530

    function

--- 18-functions.sh
+++ 18-functions.sh
@@ -9,9 +9,20 @@
 
 #Printing number
 
+# add()
+# {
+#     echo "Printing number is: $1"
+# }
+
+# add 2
+
+#printing number with read 
+
 add()
 {
-    echo "Printing number is: $1"
-}
+    echo "print number is:$1"
 
-add 2
+}
+read add
+
+add

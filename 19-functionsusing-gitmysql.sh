031f483baac95b324d7e66c4c686dbb77d3a0f80
Naveen <naveenkanaparthi09@gmail.com>
2026-04-02 08:19:37 +0530

    functions install git and mysql

--- 19-functionsusing-gitmysql.sh
+++ 19-functionsusing-gitmysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USERID=$(id -u)
+USERID=$(id -u) #this code for checking git is there or not in packages 
 VALIDATE()
 {
     #echo "exist nubmer is:$1"
@@ -18,3 +18,35 @@ fi
 dnf list installed git 
 
 VALIDATE $?
+
+# now code is for install git and my sql 
+
+
+USERID=$(id -u)
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "command is...FAILD"
+        exit 1
+    else
+        echo "command is...SUCC"
+    fi
+}
+if [ $USERID -ne 0 ]
+then
+    echo "user don't have root access plz chek and use sudo"
+    exit 1
+fi
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "git is not there plz install it"
+    dnf install git -y
+    exit 1
+else
+    echo "git is installed no need to any action"
+fi
+VALIDATE $? "INstallin git"
+
+

95b0c097c3affc644cd2a075bdf0cff934d22964
Naveen <naveenkanaparthi09@gmail.com>
2026-04-02 07:08:30 +0530

    functions install git and mysql

--- 19-functionsusing-gitmysql.sh
+++ 19-functionsusing-gitmysql.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-
+USERID=$(id-u)
 VALIDATE()
 {
-    echoo "exist nubmer is:$1"
+    #echo "exist nubmer is:$1"
+    if [ $1 -ne 0 ] #manam instal chesena dani succ full ga checsama leda ani chepatham this function
+    then
+        echo "command is ... FAILD"
+    else
+        echo "command is .... SUCC"
 }
+if [ $USERID -ne 0 ]
+then
+    echo "please check user don't root access plz use sudo"
+    exit 1
+fi
+dnf list installed git 
+
 VALIDATE $?

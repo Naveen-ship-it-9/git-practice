f415ce2b5dbb7915e8e5d7a5948146467fb73a27
Naveen <naveenkanaparthi09@gmail.com>
2026-04-04 17:00:51 +0530

    Basic loop code

--- 22-basic-loopcode.sh
+++ 22-basic-loopcode.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
-echo "8"
-echo "9"
-echo "10"
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
 
+# now anothre way like loops
+
+for i in 0 1 2 3 4 5 6 7 8 9 10
+do
+    echo "$i"
+done
 

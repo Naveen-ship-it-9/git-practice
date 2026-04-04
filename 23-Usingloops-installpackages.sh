8c764fbca064aa1b3439bc170c4ad5129ffa0a59
Naveen <naveenkanaparthi09@gmail.com>
2026-04-04 20:54:31 +0530

    using loops insatlling all packages

--- 23-Usingloops-installpackages.sh
+++ 23-Usingloops-installpackages.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+# USERID=$(id -u)
+# G="\e[31m"
+# R="\e[32m"
+# N="\e[0m"
+# CHECK_ROOT()
+# {
+#     if [ $USERID -ne 0 ]
+#     then
+#         echo "user dont have root access plz check it...."
+#         exit 1
+#     else
+#         echo "user have root access..."
+#     fi
+# }
+# VALIDATE()
+# {
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is.... $R Failed $N"
+#         exit 1
+#     else
+#         echo -e "$2 is.... $G Succ $N"
+#     fi
+# }
+# CHECK_ROOT
+# for package in $@
+# do
+#     echo "$package"
+# done
+
 USERID=0    #$(id -u)
 G="\e[31m"
 R="\e[32m"
@@ -27,44 +57,14 @@ VALIDATE()
 CHECK_ROOT
 for package in $@
 do
-    echo "$package"
+    dnf list inatalled $PACKAGE
+    if [ $? -ne 0 ]
+    then
+        echo "$PACKAGE not insatlled going to insatll it....."
+        dnf install $PACKAGE -y
+        VALIDATE $? "Insalling $PACKAGE"
+    else
+        echo "$PACKAGE insatlled no need to do any thing..."
+    fi
 done
-
-# USERID=0    #$(id -u)
-# G="\e[31m"
-# R="\e[32m"
-# N="\e[0m"
-# CHECK_ROOT()
-# {
-#     if [ $USERID -ne 0 ]
-#     then
-#         echo "user dont have root access plz check it...."
-#         exit 1
-#     else
-#         echo "user have root access..."
-#     fi
-# }
-# VALIDATE()
-# {
-#     if [ $1 -ne 0 ]
-#     then
-#         echo -e "$2 is.... $R Failed $N"
-#         exit 1
-#     else
-#         echo -e "$2 is.... $G Succ $N"
-#     fi
-# }
-# CHECK_ROOT
-# for package in $@
-# do
-#     dnf list inatalled $PACKAGE
-#     if [ $? -ne 0 ]
-#     then
-#         echo "$PACKAGE not insatlled going to insatll it....."
-#         dnf install $PACKAGE -y
-#         VALIDATE $? "Insalling $PACKAGE"
-#     else
-#         echo "$PACKAGE insatlled no need to do any thing..."
-#     fi
-# done
 

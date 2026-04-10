9414ff4945dc321da6cc0f85729aba2fc0e57ef9
Naveen <naveenkanaparthi09@gmail.com>
2026-04-10 13:44:01 +0530

    redirectors

--- 24-redirectors.sh
+++ 24-redirectors.sh
@@ -12,20 +12,34 @@ CHECK_ROOT()
 {
     if [ $userid -ne 0 ]
     then
-        echo -e "$R user dont have root access plz cehck... $N"
+        echo -e "$R user dont have root access plz cehck... $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$R user have root access...$N"
+        echo -e "$R user have root access...$N" &>>$LOG_FILE
     fi
 }
 VALIDATE()
 {
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is ...$R Failed $N"
+        echo -e "$2 is ...$R Failed $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is ... $G Succ $N"
+        echo -e "$2 is ... $G Succ $N" &>>$LOG_FILE
     fi
 }
 CHECK_ROOT
+for package in $@
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+        echo -e "$R $package not installed...going to install it.... $N" &>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
+        exit 1
+        VALIDATE $? "package installing"
+    else
+        echo -e "$G $pacake is installed noting to do.... $N" &>>$LOG_FILE
+    fi
+done
+

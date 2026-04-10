67fe05b53ed5dc97c2388f0aea5ce7ed5d202953
Naveen <naveenkanaparthi09@gmail.com>
2026-04-10 11:47:45 +0530

    redirectors

--- 23-Usingloops-installpackages.sh
+++ 23-Usingloops-installpackages.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-USERID=$(id -u)
-G="\e[31m"
-R="\e[32m"
-N="\e[0m"
-CHECK_ROOT()
-{
-    if [ $USERID -ne 0 ]
-    then
-        echo "user dont have root access plz check it...."
-        exit 1
-    else
-        echo "user have root access..."
-    fi
-}
-VALIDATE()
-{
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is.... $R Failed $N"
-        exit 1
-    else
-        echo -e "$2 is.... $G Succ $N"
-    fi
-}
-CHECK_ROOT
-for package in $@
-do
-    echo "$package"
-done
-
 # USERID=$(id -u)
 # G="\e[31m"
 # R="\e[32m"
@@ -55,17 +25,47 @@ done
 #     fi
 # }
 # CHECK_ROOT
-# #sh.usingloops-installingpackages.sh git mysql postfix nginx
-# for package in $@  #$@ means all argements pass to it
+# for package in $@
 # do
-#     dnf list inatalled $PACKAGE
-#     if [ $? -ne 0 ]
-#     then
-#         echo "$PACKAGE not insatlled going to insatll it....."
-#         dnf install $PACKAGE -y
-#         VALIDATE $? "Insalling $PACKAGE"
-#     else
-#         echo "$PACKAGE insatlled no need to do any thing..."
-#     fi
+#     echo "$package"
 # done
+
+USERID=$(id -u)
+G="\e[31m"
+R="\e[32m"
+N="\e[0m"
+CHECK_ROOT()
+{
+    if [ $USERID -ne 0 ]
+    then
+        echo "user dont have root access plz check it...."
+        exit 1
+    else
+        echo "user have root access..."
+    fi
+}
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.... $R Failed $N"
+        exit 1
+    else
+        echo -e "$2 is.... $G Succ $N"
+    fi
+}
+CHECK_ROOT
+#sh.usingloops-installingpackages.sh git mysql postfix nginx
+for package in $@  #$@ means all argements pass to it
+do
+    dnf list inatalled $PACKAGE
+    if [ $? -ne 0 ]
+    then
+        echo "$PACKAGE not insatlled going to insatll it....."
+        dnf install $PACKAGE -y
+        VALIDATE $? "Insalling $PACKAGE"
+    else
+        echo "$PACKAGE insatlled no need to do any thing..."
+    fi
+done
 

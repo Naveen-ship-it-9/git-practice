0cd79b1197c59f23acdb64e2a87fa06d335b2162
Naveen <naveenkanaparthi09@gmail.com>
2026-04-20 15:40:26 +0530

    deleteinglogs

--- 25-deletelog.sh
+++ 25-deletelog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SOURCE_DIR=/c/Users/kanap/logs
+SOURCE_DIR="/c/Users/kanap/logs"
 if [ -d $SOURCE_DIR ]
 then
     echo "$SOURCE_DIR exists"
@@ -7,6 +7,6 @@ else
     echo "$SOURCE_DIR does not exists"
     exit 1
 fi
-FILES=$(find . -name "*.log" -mtime +14)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "FILES: $FILES"
 

6af6f0ec5f08294cd030a075e790c373f2dcc2e5
Naveen <naveenkanaparthi09@gmail.com>
2026-04-21 16:38:43 +0530

    deletelog

--- 25-deletelog.sh
+++ 25-deletelog.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# SOURCE_DIR=/c/Users/kanap/logs
-# if [ -d $SOURCE_DIR ]
-# then
-#     echo "$SOURCE_DIR exists"
-# else
-#     echo "$SOURCE_DIR does not exists"
-#     exit 1
-# fi
-# FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +14)
-# echo "files: $FILES"
-# while IFS= read -r line
-# do
-#     echo "Deleting line: $line"
-#     rm -rf $line
-# done <<< $FILES
-
-
 SOURCE_DIR=/c/Users/kanap/logs
 if [ -d $SOURCE_DIR ]
 then
@@ -26,9 +9,26 @@ else
 fi
 FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +14)
 echo "files: $FILES"
-while IFS= read -r file
+while IFS= read -r line
 do
-    echo "Deleting file: $files"
-    rm -rf $file
+    echo "Deleting line: $line"
+    rm -rf $line
 done <<< $FILES
+
+
+# SOURCE_DIR=/c/Users/kanap/logs
+# if [ -d $SOURCE_DIR ]
+# then
+#     echo "$SOURCE_DIR exists"
+# else
+#     echo "$SOURCE_DIR does not exists"
+#     exit 1
+# fi
+# FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +14)
+# echo "files: $FILES"
+# while IFS= read -r file
+# do
+#     echo "Deleting file: $files"
+#     rm -rf $file
+# done <<< $FILES
 

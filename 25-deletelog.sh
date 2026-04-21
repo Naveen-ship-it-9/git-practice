7d7829d01ccb68fc6030458ca8bb044dee4fde69
Naveen <naveenkanaparthi09@gmail.com>
2026-04-21 16:07:12 +0530

    backup

--- 25-deletelog.sh
+++ 25-deletelog.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# SOURCE_DIR=/c/Users/kanap/logs
+# if [ -d $SOURCE_DIR ]
+# then
+#     echo "$SOURCE_DIR exists"
+# else
+#     echo "$SOURCE_DIR does not exists"
+#     exit 1
+# fi
+# FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +14)
+# echo "files: $FILES"
+# while IFS= read -r line
+# do
+#     echo "Deleting line: $line"
+#     rm -rf $line
+# done <<< $FILES
+
+
 SOURCE_DIR=/c/Users/kanap/logs
 if [ -d $SOURCE_DIR ]
 then
@@ -7,11 +24,11 @@ else
     echo "$SOURCE_DIR does not exists"
     exit 1
 fi
-FILES=$(find "$SOURCE_DIR" -name "*.log")
+FILES=$(find "$SOURCE_DIR" -name "*.log" -mtime +14)
 echo "files: $FILES"
-while IFS= read -r line
+while IFS= read -r file
 do
-    echo "Deleting line: $line"
-    rm -rf $line
+    echo "Deleting file: $files"
+    rm -rf $file
 done <<< $FILES
 
---
+++ 26-Backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+#$3 if user not meantion how may days then we take it has 14 days
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+USAGE()
+{
+    echo -e "$Y $USAGE:: $N sh filename.sh <source dir> <destination dir> <days(optinal)>"
+
+}
+#check source and destitation are provided or not
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+#now checking sourcedir and destination dir
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$R $SOURCE_DIR exist $N"
+else
+    echo -e "$G $SOURCE_DIR does not exist...plz check $N"
+    exit 1
+fi
+if [ -d $DEST_DIR ]
+then
+    echo -e "$Y $DEST_DIR is exist $N"
+else
+    echo -e "$G $DEST_DIR is not exists plz check $N"
+    exit 1
+fi
+
+
+
